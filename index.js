d4d1ca66d81b41ddc8b66d76f690a2c3cfac12ca
zayadur <zayadur@outlook.com>
2022-04-06 17:13:27 -0400

    Update projects styling and add details

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 
 /* make nav link "active" on view */
 
+function highlightLinks(event, linkName) {
+    var i, navlink;
+
+    // reset all links to in-"active"
+    link = document.getElementsByClassName("navlink");
+    for(i = 0; i < linkName.length; i++) {
+        link[i].className = link[i].className.replace(" active", "");
+    }
+
+    document.getElementByClass
+}
+
 /* tabbed view for projects section */
 function openProjects(event, projectName) {
-    var i, tabcontent, tablinks;
+    var i, description, tab;
 
-    // hide all but first tab by default
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+    // hide all tabs by default
+    description = document.getElementsByClassName("description");
+    for (i = 0; i < description.length; i++) {
+        description[i].style.display = "none";
     }
 
     // set all tabs to in-"active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tab = document.getElementsByClassName("tab");
+    for (i = 0; i < tab.length; i++) {
+        tab[i].className = tab[i].className.replace(" active", "");
     }
 
     // show and make clicked tab "active"
